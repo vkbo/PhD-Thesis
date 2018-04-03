6a5f8d5b8a8b0051cffddae1550aff5ecae56075
Veronica K. B. Olsen <v.k.b.olsen@fys.uio.no>
2018-04-03 17:04:45 +0200

    Added self modulation plots

--- Scripts/phdSelfModulationSim.m
+++ Scripts/phdSelfModulationSim.m
@@ -3,9 +3,14 @@
 % Plasma Regimes
 %
 
-fMain = figure(1); clf;
+oData = OsirisData;
+
+fMain = figure(40); clf;
 fMain.Units = 'Pixels';
-fFigureSize(fMain,[1600 500]);
+fFigureSize(fMain,[800 480]);
+
+aAxes(1) = axes('Units','Pixels','Position',[55 288 736 164]);
+aAxes(2) = axes('Units','Pixels','Position',[55  48 736 164]);
 
 set(fMain,'defaultUicontrolFontName','DejaVu Sans');
 set(fMain,'defaultUitableFontName','DejaVu Sans');
@@ -13,9 +18,27 @@ set(fMain,'defaultAxesFontName','DejaVu Sans');
 set(fMain,'defaultTextFontName','DejaVu Sans');
 set(fMain,'defaultUipanelFontName','DejaVu Sans');
 
-oData = OsirisData;
-oData.Path = 'PPE-S01';
+oData.Path = '/data/SimData/S-Series/PPE-S01';
 
-fPlotParticleDensity(oData,42,'PB','IsSubplot','Yes','ShowOverlay','No','Limits',[150 315 -1 1]);
-title('')
+axes(aAxes(1));
+fPlotParticleDensity(oData,0,'PB','Limits',[40 316 -1 1],'IsSubplot','Yes','HideDump','Yes');
+hT = get(gca,'Title');
+set(hT,'FontWeight','Normal');
+
+axes(aAxes(2));
+fPlotParticleDensity(oData,30,'PB','Limits',[40 316 -1 1],'IsSubplot','Yes','HideDump','Yes');
+hT = get(gca,'Title');
+set(hT,'FontWeight','Normal');
+
+
+fMain = figure(41); clf;
+fMain.Units = 'Pixels';
+fFigureSize(fMain,[800 370]);
+
+aAxes(1) = axes('Units','Pixels','Position',[55 48 740 300]);
+
+axes(aAxes(1));
+fPlotParticleDensity(oData,42,'PB','Limits',[150 315 -1 1],'IsSubplot','Yes','ShowOverlay','No','HideDump','Yes');
 caxis([0 15])
+hT = get(gca,'Title');
+set(hT,'FontWeight','Normal');
